ef411fc39eb9ca01e2fe450a69b991e3a8dba5d8
Afrad Ahsan <afradahsan2002@gmail.com>
2023-12-29 21:42:49 +0530

    First

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:chatgpt/view/screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat GPT',
+      home: HomePage(),
+    );
+  }
+}
