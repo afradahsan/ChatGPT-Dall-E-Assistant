6b3f69b9e04ed6fc2fd4eb93944dec8166088d7d
Afrad Ahsan <afradahsan2002@gmail.com>
2023-12-31 14:52:52 +0530

    Secret

--- lib/model/openai_services.dart
+++ lib/model/openai_services.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 
-import 'package:chatgpt/view/constants/.gitignore';
+import 'package:chatgpt/view/constants/secret.dart';
 import 'package:http/http.dart' as http;
 
 class OpenAIService{
