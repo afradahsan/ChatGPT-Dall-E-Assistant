ccc4edfacd56eccd9fa12c6beb3d678a95117e53
Afrad Ahsan <afradahsan2002@gmail.com>
2023-12-31 12:24:58 +0530

    speechtotextpub

--- lib/view/screens/home.dart
+++ lib/view/screens/home.dart
@@ -18,8 +18,6 @@ class _HomePageState extends State<HomePage> {
     double screenWidth = MediaQuery.sizeOf(context).width;
     double screenHeight = MediaQuery.sizeOf(context).height;
 
-    print(screenHeight);
-
     return Scaffold(
       backgroundColor: Palette.gptbackground,
       appBar: AppBar(title: const Text('ChatGPT', style: TextStyle(color: Colors.white),), centerTitle: true, backgroundColor: Palette.transparent, elevation: 0,),
@@ -29,9 +27,20 @@ class _HomePageState extends State<HomePage> {
           height: screenHeight,
           width: screenWidth,
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
+              SizedBox(height: screenHeight/27,),
+              // ClipRRect(
+              //   borderRadius: BorderRadius.circular(20),
+              //   child: Image.asset('assets/images/ChatGPT-Logo.jpg', height: 40, )),
+              Text('Hey, How can I help you?', style: TextStyle(color: Colors.white),),
+              // Container(
+              //   decoration: BoxDecoration(color: Palette.transparent, border: Border.all(color: Colors.white), borderRadius: BorderRadius.circular(10)),
+              //   child: Padding(
+              //     padding: const EdgeInsets.all(8.0),
+              //     child: Text('Help me Pick\nan outfit that looks good on camera'),
+              //   ),
+              // ),
               Stack(
                 children: [
                   Row(
@@ -39,8 +48,11 @@ class _HomePageState extends State<HomePage> {
                       Container(
                         height: screenHeight/15,
                         width: screenWidth/1.27,
-                        child: TextField(
+                        child: TextFormField(
+                          style: TextStyle(color: Colors.white),
                           decoration: InputDecoration(
+                            hintText: 'Enter your Prompt Here',
+                            hintStyle: TextStyle(color: const Color.fromARGB(115, 255, 255, 255)),
                             focusedBorder: OutlineInputBorder(
                               borderSide: BorderSide(color: Colors.white)
                             ),
@@ -56,7 +68,7 @@ class _HomePageState extends State<HomePage> {
                         height: screenHeight/25.6,
                         width: screenHeight/25.6,
                         decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(20)),
-                        child: Icon(Icons.mic, color: Palette.gptbackground, size: 10,)),
+                        child: Icon(Icons.mic, color: Palette.gptbackground, size: 17,)),
                     ],
                   )
                 ],
